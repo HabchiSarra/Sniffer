83385795788fbeb3734a396d189e055317f3ff46
Antoine Veuiller <aveuiller@gmail.com>
2018-03-27 09:33:32 +0200

    feat: add a column to merging

--- HarissaProject/harissa_project_analysis/binding/merge.py
+++ HarissaProject/harissa_project_analysis/binding/merge.py
@@ -51,8 +51,9 @@ class CommitNumberIRDCsvWriter(CsvOutputWriter):
     NUMBER_COMMITS_I = "number_commits_I"
     NUMBER_COMMITS_R = "number_commits_R"
     NUMBER_COMMITS_D = "number_commits_D"
+    NUMBER_COMMITS_I_AND_D = "number_commits_introducing_and_removing"
     HEADER = (
-        PROJECT, NUMBER_COMMITS, NUMBER_COMMITS_I, NUMBER_COMMITS_R, NUMBER_COMMITS_D
+        PROJECT, NUMBER_COMMITS, NUMBER_COMMITS_I, NUMBER_COMMITS_R, NUMBER_COMMITS_D, NUMBER_COMMITS_I_AND_D
     )
 
     def __init__(self, out_path):
@@ -62,12 +63,14 @@ class CommitNumberIRDCsvWriter(CsvOutputWriter):
         """
         super().__init__(self.HEADER, output_path=out_path)
 
-    def add_project(self, name: str, nb_commits: int, nb_commits_i: int, nb_commits_r: int, nb_commits_d: int):
+    def add_project(self, name: str, nb_commits: int, nb_commits_i: int, nb_commits_r: int, nb_commits_d: int,
+                    nb_i_and_d):
         self._add_line({
             CommitNumberIRDCsvWriter.PROJECT: name,
             CommitNumberIRDCsvWriter.NUMBER_COMMITS_I: nb_commits_i,
             CommitNumberIRDCsvWriter.NUMBER_COMMITS_R: nb_commits_r,
             CommitNumberIRDCsvWriter.NUMBER_COMMITS_D: nb_commits_d,
+            CommitNumberIRDCsvWriter.NUMBER_COMMITS_I_AND_D: nb_i_and_d,
             CommitNumberIRDCsvWriter.NUMBER_COMMITS: nb_commits
         })
 
@@ -96,17 +99,27 @@ def merge_commits_by_ird(input_metrics: str):
             nb_refactoring = 0
             nb_deletion = 0
             nb_commits = 0
+            # Remove is delete or refactor
+            introduce_and_remove = 0
             reader = csv.DictReader(input_file)
             for line in reader:
                 nb_commits += 1
                 values = list(line.values())
+                introduced = False
+                removed = False
                 if [x for x in values[3::3] if is_not_zero(x)]:
                     nb_introduction += 1
+                    introduced = True
                 if [x for x in values[4::3] if is_not_zero(x)]:
                     nb_refactoring += 1
+                    removed = True
                 if [x for x in values[5::3] if is_not_zero(x)]:
                     nb_deletion += 1
-            csv_writer.add_project(folder, nb_commits, nb_introduction, nb_refactoring, nb_deletion)
+                    removed = True
+                if introduced and removed:
+                    introduce_and_remove += 1
+            csv_writer.add_project(folder, nb_commits, nb_introduction, nb_refactoring, nb_deletion,
+                                   introduce_and_remove)
     csv_writer.write()
 
 
@@ -128,6 +141,6 @@ def merge_ird_by_smell(input_metrics: str):
 
 if __name__ == '__main__':
     metrics = "/data/tandoori-metrics/results"
-    # merge_commits_by_ird(metrics)
-    merge_ird_by_smell(metrics)
     # input_commits = "/data/tandoori-metrics/commits-analysis"
+    merge_commits_by_ird(metrics)
+    # merge_ird_by_smell(metrics)
